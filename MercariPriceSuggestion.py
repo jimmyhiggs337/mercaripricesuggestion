b69bc7363a1107b59274b4276317585bc7c71bf6
jimmyhiggs337 <jimmyhiggs337@gmail.com>
2019-11-16 18:57:43 -0600

    added data normalization

--- MercariPriceSuggestion.py
+++ MercariPriceSuggestion.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#*******************Imports**********************************************
 import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
@@ -12,17 +13,14 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.preprocessing import LabelBinarizer
 # Ridge - Reduces multicollinearity in regression. Applies L2 Regularization
 from sklearn.linear_model import Ridge
-import Normailizer
+import Normalizer
+#*******************Stop Imports**********************************************
 
-#________________________Import Data________________________
-train = pd.read_csv('train.tsv', sep = '\t')
-train.head()
-test = pd.read_csv('test.tsv', sep = '\t',engine = 'python')
-combined = pd.concat([train,test])
-submission = test[['test_id']]
-train_size = len(train)
-#combined_ML = combined.sample(frac=0.1).reset_index(drop=True)
 
+
+
+
+#*******************Functions*************************************************
 
 #________________________Ridge Recression Function____________________
 #calculates error of MLA
@@ -30,12 +28,52 @@ def rmsle(y, y1):
     assert len(y) == len(y1)
     return np.sqrt(np.mean(np.power(np.log1p(y)-np.log1p(y1), 2)))
 
+#***********************Stop Functions****************************************
+    
+
+
+
+
+
+#*******************   Main    ************************************************
+
+#________________________Import Data________________________
+train = pd.read_csv('train.tsv', sep = '\t')
+train.head()
+test = pd.read_csv('test.tsv', sep = '\t',engine = 'python')
+combined = pd.concat([train,test])
+submission = test[['test_id']]
+trainSize = len(train)
+
+#________________________Data Normailization________________________
+
+#removing missing values
+Normalizer.missingValues(combined,'brand_name', 'None')
+Normalizer.missingValues(combined,'item_description', 'None')
+Normalizer.missingValues(combined,'category_name', 'missing')
+
+combined.item_description = combined.item_description.astype(str)
+
+#removing punctuation from item description
+combined.item_description = combined['item_description'].apply(Normalizer.removePunc)
+
+#removing stop words from item description
+combined.item_description = combined['item_description'].apply(Normalizer.removeStopWords)
+
+#setting words to lowercase
+combined.item_description = combined['item_description'].apply(Normalizer.toLower)
+
+#combined_ML = combined.sample(frac=0.1).reset_index(drop=True)
+
+
+
+
 #________________________Count Vectorizer___________________
 # Count Vectorizer - counts word frequencies 
 cv = CountVectorizer()
 catName = cv.fit_transform(combined['category_name'])
-#X_sub1 = cv.fit_transform(combined['sub_category_1'])
-#X_sub2 = cv.fit_transform(combined['sub_category_2'])
+#subCat1 = cv.fit_transform(combined['sub_category_1'])
+#subCat2 = cv.fit_transform(combined['sub_category_2'])
 #X_category
 cv = CountVectorizer(min_df=10)
 name = cv.fit_transform(combined['name'])
@@ -53,4 +91,9 @@ itemDesc = tv.fit_transform(combined['item_description'])
 # Label Binarizer - Converts labels into numerical representation "a,b,c" -> [1,2,3]
 lb = LabelBinarizer(sparse_output=True)
 brand = lb.fit_transform(combined['brand_name'])
+
+#*******************   Stop Main    ******************************************
+
+
+
 
